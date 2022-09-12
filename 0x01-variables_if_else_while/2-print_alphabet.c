f7d716cbfb5f3695ba823be8e231b1660fcbf75e
LAWRENCEALX <zamorrrawurld7@gmail.com>
2022-09-12 12:45:30 -0700

    aplhbat

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,7 +5,7 @@
  * Return: Always 0 (Success)
 */
 
-main(void)
+int main(void)
 {
 	char letter;
 
---
+++ 0x01-variables_if_else_while/4-print_alphbat.c
@@ -0,0 +1,20 @@
+# include <stdio.h>
+/*
+ * print letters of alphabet
+ */
+void main)
+{
+	int i = 97;
+
+	while (i < 123)
+	{
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
+		i++;
+	}
+	purchar(10);
+
+	return (0);
+}
