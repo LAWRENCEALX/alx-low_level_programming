1246f85b223820559d4a25ad684ccaff6e279088
LAWRENCEALX <zamorrrawurld7@gmail.com>
2022-09-13 07:58:18 -0700

    function and nnested loop

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <main.h>
+#include <_putchar.c>
+#include <unistd.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	_putchar("_putchar");
+	_putchar('\n');
+
+	return (0);
+}
