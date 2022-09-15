37788b3b610089e6804c92e5109109f647e0be1a
LAWRENCEALX <zamorrrawurld7@gmail.com>
2022-09-15 13:45:09 -0700

    TASK 4: exclude 2 & 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - function to print numbers
+ * @c: integer
+ * Return: Alaways 0
+ */
+void print_most_numbers(void)
+{
+	int c;
+	c = 48;
+
+	for (c = 48; c <= 57; c++)
+	{
+		if (c != 50 && c != 52)
+			_putchar(c);
+	}
+	_putchar('\n');
+}
