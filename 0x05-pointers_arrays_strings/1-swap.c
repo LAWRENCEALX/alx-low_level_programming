5f061bd925fff73c74494a12ec51a2a005d1010f
LAWRENCEALX <zamorrrawurld7@gmail.com>
2022-09-19 14:21:06 -0700

    TASK 2: SWAP NU

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,8 +2,8 @@
 #include <limits.h>
 /**
  * swap_int - function to swap numbers
- * @*a: pointer of  swapped numbers
- * @*b: pointer of swap number
+ * @a: pointer of  swapped numbers
+ * @b: pointer of swap number
  * Return: always 0
  */
 void swap_int(int *a, int *b)
